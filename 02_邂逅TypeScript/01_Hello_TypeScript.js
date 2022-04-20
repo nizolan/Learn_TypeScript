258dd7a73046891b5eea482483ea700677fa5b87
nizolan <nizolan@163.com>
2022-04-20 16:08:17 +0800

    first add

---
+++ 02_邂逅TypeScript/01_Hello_TypeScript.js
@@ -0,0 +1,7 @@
+var message = "Hello TypeScript";
+// message = 123;
+function foo(payload) {
+    console.log(payload.length);
+}
+// foo(123);
+foo("abc");
---
+++ 02_邂逅TypeScript/01_Hello_TypeScript.ts
@@ -0,0 +1,10 @@
+let message: string = "Hello TypeScript";
+
+// message = 123;
+
+function foo(payload: string) {
+    console.log(payload.length);
+}
+
+// foo(123);cd
+foo("abc");
